290e428fc26a6618b5f3624f4f3c0b09ae3a41d9
Michikatsu500 <143003304+Michikatsu500@users.noreply.github.com>
2025-06-23 08:13:00 -0300

    dia 23/06

--- main.js
+++ main.js
@@ -1,54 +1,22 @@
-const numeroSenha = document.querySelector('.parametro-senha_texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.parametro-senha_botao');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
-    tamanhoSenha = tamanhoSenha - 1;
-    numeroSenha.textContent = tamanhoSenha;
-}
-
-function aumentaTamanho() {
-    tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha--;
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
 
 function aumentaTamanho() {
     if (tamanhoSenha < 20){
-        tamanhoSenha = tamanhoSenha + 1;
+        tamanhoSenha = tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
