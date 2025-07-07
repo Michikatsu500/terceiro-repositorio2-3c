aadd956277fd11fade58e0223740149f0cf9347f
Michikatsu500 <143003304+Michikatsu500@users.noreply.github.com>
2025-07-07 08:57:01 -0300

    dia 7/7 - save 4

--- main.js
+++ main.js
@@ -58,16 +58,16 @@ function geraSenha() {
 }
 
 function classificaSenha(tamanhoAlfabeto) {
-    const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
     let entropia = tamanhoSenha*Math.log2(tamanhoAlfabeto);
     console.log(entropia);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 12) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
